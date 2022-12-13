c7ec2858e752f979c9a795f7a0853e383ae93b0b
M Zubair Naeem <zubairnaeem45@gmail.com>
2022-12-13 19:27:13 +0500

    ui changes

--- app/src/main/java/com/example/emotiondetector/facedetector/AudioPlayerActivity.java
+++ app/src/main/java/com/example/emotiondetector/facedetector/AudioPlayerActivity.java
@@ -49,6 +49,7 @@ public class AudioPlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_audio_player);
+        getSupportActionBar().hide();
         getIntentData();
         setMoodName();
         bindViews();
--- app/src/main/java/com/example/emotiondetector/facedetector/CameraFragment.java
+++ app/src/main/java/com/example/emotiondetector/facedetector/CameraFragment.java
@@ -1,5 +1,6 @@
 package com.example.emotiondetector.facedetector;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -13,6 +14,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
